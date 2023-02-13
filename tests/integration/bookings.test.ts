52013b9c723bfee1090aeb04797304dd63b00b36
higorpr <higorpr@gmail.com>
2023-02-13 15:02:55 -0300

    feat: complete PUT /booking routes testing

--- tests/integration/bookings.test.ts
+++ tests/integration/bookings.test.ts
@@ -264,7 +264,7 @@ describe("POST /booking", () => {
                 .set("Authorization", `Bearer ${token}`);
             expect(response.statusCode).toBe(403);
         });
-        it("should return create new booking entry", async () => {
+        it("should create new booking entry", async () => {
             // create user and valid session
             const user = await createUser();
             const token = await generateValidToken(user);
@@ -324,6 +324,134 @@ describe("PUT /booking", () => {
         });
     });
     describe("when token is valid", () => {
-        // it("should return status code 200 AND ... ", async () => {});
+        it("should return status code 200 and return bookingId ", async () => {
+            // create user and valid session
+            const user = await createUser();
+            const token = await generateValidToken(user);
+            await createSession(token);
+
+            // create context
+            const hotel = await createHotel();
+            const room1 = await createRoom(hotel.id, 2);
+            const room2 = await createRoom(hotel.id, 2);
+            const oldBooking = await createBooking(user.id, room1.id);
+
+            // test
+            const body = { roomId: room2.id };
+            const response = await server
+                .put(`/booking/${oldBooking.id}`)
+                .send(body)
+                .set("Authorization", `Bearer ${token}`);
+            expect(response.statusCode).toBe(200);
+            expect(response.body).toEqual({
+                bookingId: expect.any(Number),
+            });
+        });
+        it("should return status code 404 when roomId sent on body is non-existant", async () => {
+            // create user and valid session
+            const user = await createUser();
+            const token = await generateValidToken(user);
+            await createSession(token);
+
+            // create context
+            const hotel = await createHotel();
+            const room1 = await createRoom(hotel.id, 2);
+            const room2 = await createRoom(hotel.id, 2);
+            const oldBooking = await createBooking(user.id, room1.id);
+
+            // test
+            const body = { roomId: 0 };
+            const response = await server
+                .put(`/booking/${oldBooking.id}`)
+                .send(body)
+                .set("Authorization", `Bearer ${token}`);
+            expect(response.statusCode).toBe(404);
+        });
+        it("should return status code 404 when bookingId sent on query parameters is non-existant", async () => {
+            // create user and valid session
+            const user = await createUser();
+            const token = await generateValidToken(user);
+            await createSession(token);
+
+            // create context
+            const hotel = await createHotel();
+            const room1 = await createRoom(hotel.id, 2);
+            const room2 = await createRoom(hotel.id, 2);
+            const oldBooking = await createBooking(user.id, room1.id);
+
+            // test
+            const body = { roomId: room2.id };
+            const response = await server
+                .put(`/booking/${0}`)
+                .send(body)
+                .set("Authorization", `Bearer ${token}`);
+            expect(response.statusCode).toBe(404);
+        });
+
+        it("should return status code 403 when roomId sent is at capacity", async () => {
+            // create user and valid session
+            const user = await createUser();
+            const token = await generateValidToken(user);
+            await createSession(token);
+
+            // create context
+            const hotel = await createHotel();
+            const room1 = await createRoom(hotel.id, 2);
+            const room2 = await createRoom(hotel.id, 0);
+            const oldBooking = await createBooking(user.id, room1.id);
+
+            // test
+            const body = { roomId: room2.id };
+            const response = await server
+                .put(`/booking/${oldBooking.id}`)
+                .send(body)
+                .set("Authorization", `Bearer ${token}`);
+            expect(response.statusCode).toBe(403);
+        });
+        it("should return status code 403 when user does not have a booking on their id", async () => {
+            // create user and valid session
+            const user = await createUser();
+            const token = await generateValidToken(user);
+            await createSession(token);
+            const bookingUser = await createUser();
+
+            // create context
+            const hotel = await createHotel();
+            const room1 = await createRoom(hotel.id, 2);
+            const room2 = await createRoom(hotel.id, 2);
+            const oldBooking = await createBooking(bookingUser.id, room1.id);
+
+            // test
+            const body = { roomId: room2.id };
+            const response = await server
+                .put(`/booking/${oldBooking.id}`)
+                .send(body)
+                .set("Authorization", `Bearer ${token}`);
+            expect(response.statusCode).toBe(403);
+        });
+
+        it("should alter the booking entry sent on query parameters", async () => {
+            // create user and valid session
+            const user = await createUser();
+            const token = await generateValidToken(user);
+            await createSession(token);
+
+            // create context
+            const hotel = await createHotel();
+            const room1 = await createRoom(hotel.id, 2);
+            const room2 = await createRoom(hotel.id, 2);
+            const oldBooking = await createBooking(user.id, room1.id);
+
+            // test
+            const body = { roomId: room2.id };
+            const response = await server
+                .put(`/booking/${oldBooking.id}`)
+                .send(body)
+                .set("Authorization", `Bearer ${token}`);
+            const check = await prisma.booking.findFirst({
+                where: { id: response.body.bookingId },
+            });
+            expect(check.roomId).toBe(room2.id);
+        });
     });
 });
